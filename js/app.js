2d18843151a14bc16ab24bc9879e457ab1d76ec1
Waheed <wahid.khan74@gmail.com>
2022-12-02 17:26:32 +0000

    added data varaible

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+var data = {
+  "menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}
+};
+
 function toCelsius(fahrenheit) {
     return (5/9) * (fahrenheit-32);
   }
--- js/data.js
+++ js/data.js
@@ -1,3 +1,17 @@
 let length = 16;                               // Number
 let lastName = "Johnson";                      // String
 let x = {firstName:"John", lastName:"Doe"};  
+
+let data = {
+    "menu": {
+    "id": "file",
+    "value": "File",
+    "popup": {
+      "menuitem": [
+        {"value": "New", "onclick": "CreateNewDoc()"},
+        {"value": "Open", "onclick": "OpenDoc()"},
+        {"value": "Close", "onclick": "CloseDoc()"}
+      ]
+    }
+  }
+}
